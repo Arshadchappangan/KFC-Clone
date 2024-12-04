92f2fc419aaf970c979466cf9c99a40fe65e5e5d
Arshad Chappangan <arshadchappangan349@gmail.com>
2024-12-04 11:39:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function divtoggle(id){
+    var demo=document.getElementById(id);
+    if(demo.style.display=="none"){
+        demo.style.display="block";
+    }else{
+        demo.style.display="none";
+    }
+}
